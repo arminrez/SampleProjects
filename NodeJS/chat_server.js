5bb05c0bd873ee07394e13438ca02f4801677c71
arminrez <armin_44@hotmail.com>
2014-10-13 02:36:38 -0700

    Update chat_server.js

--- NodeJS/chat_server.js
+++ NodeJS/chat_server.js
@@ -4,7 +4,7 @@ fs = require('fs');
 
 var server = http.createServer(function (req, res) {
 
-	var path = url.parse(req.url).pathname; 
+    var path = url.parse(req.url).pathname; 
 	
     switch (path){
 	case '/':
